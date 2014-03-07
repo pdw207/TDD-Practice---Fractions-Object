560fc5589ee46a90700323e78ac3f61cc27ebe7a
Greg Towne <townegr@yahoo.com>
2014-03-07 13:32:33 -0500

    First tests

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,12 +1,23 @@
 require 'csv'
 
 class Employee
+	attr_reader :first_name, :last_name, :annual_income, :paid_tax, :tax_rate, :calc_tax
 
   def initialize(hash)
-    # @name = hash[:first_name] + hash[:last_name]
+    @first_name = hash["first_name"]
+    @last_name  = hash["last_name"]
+    @annual_income = hash["annual_income"].to_i
+    @paid_tax = hash["paid_tax"].to_i 
+    @tax_rate = (hash["tax_rate"].to_f / 100)
   end
+
+ 	def calc_tax
+ 		(@annual_income * @tax_rate) - @paid_tax
+ 	end
+
+
 end
 
-CSV.foreach('../taxes.csv', headers: true ) do |trans|
-   employee_list = Employeer.new(row)
-end
+# CSV.foreach('../taxes.csv', headers: true ) do |trans|
+#    employee_list = Employeer.new(row)
+# end
